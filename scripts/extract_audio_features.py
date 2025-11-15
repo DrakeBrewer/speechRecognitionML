b5be88351cbb4037ee64660048217760c0af6ec7
BrownChickenBrownCow <david.birney@wsu.edu>
2025-11-14 20:16:54 -0800

    Reorganized project structure

---
+++ scripts/extract_audio_features.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import librosa
+
+PROJECT_ROOT_DIR = Path(__file__).resolve().parents[1]
+AUDIO_DIR = PROJECT_ROOT_DIR / "data" / "audio" / "raw"
+OUTPUT_PATH = PROJECT_ROOT_DIR / "data"/ "audio" / "processed" / "audio_features.csv"
+SAMPLE_RATE = 22050
+WINDOW_LEN = 1.0   			                                                            # 1 sec clip length
+HOP_LEN = 0.5      			                                                            # overlap 50%
+SILENCE_THRESHOLD = 0.01  			                                                    # skip quiet sections
+
+def extract_features(file_path, speaker):
+
+    # define librosa feature functions
+    mfcc_feat = librosa.feature.mfcc
+    chroma_feat = librosa.feature.chroma_stft
+    contrast_feat = librosa.feature.spectral_contrast
+    zcr_feat = librosa.feature.zero_crossing_rate
+    rms_feat = librosa.feature.rms
+    mfcc_delta_feat = librosa.feature.delta
+
+    # extract amplitude (raw) data from each audio file
+    raw_data, sampling_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+    window_size = int(WINDOW_LEN * sampling_rate)                       # for processing small chunks at a time
+    hop_size = int(HOP_LEN * sampling_rate)                             # overlap
+    feature_vectors = []                                                # each windows (row) extracted numerical features
+
+    frames = librosa.util.frame(raw_data, frame_length=window_size, hop_length=hop_size)
+    for i in range(frames.shape[1]):
+        window = frames[:,i]
+
+        # skip over silent segments
+        rms = rms_feat(y=window)
+        if np.mean(rms) < SILENCE_THRESHOLD:
+            continue
+
+        # extract features from each window
+        mfcc = mfcc_feat(y=window, sr=sampling_rate, n_mfcc=13)
+        chroma = chroma_feat(y=window, sr=sampling_rate)
+        contrast = contrast_feat(y=window, sr=sampling_rate)
+        zcr = zcr_feat(y=window)
+        mfcc_delta = mfcc_delta_feat(mfcc)
+
+        # combine into one feature_vector (2D -> 1D vector)
+        feature_list = [mfcc, chroma, contrast, zcr, rms, mfcc_delta]
+        features = []
+        for idx, feat in enumerate(feature_list):
+            if idx == 3 or idx == 4:
+                features.append(np.mean(feat))
+            else:
+                features.extend(np.mean(feat, axis=1))                  # 2D -> 1D
+
+        features.append(speaker)                                        # add speaker name
+        feature_vectors.append(features)
+
+    # create && return data frame
+    df = pd.DataFrame(feature_vectors)
+    return df
+
+def main():
+    data_frames = []
+    for file in AUDIO_DIR.glob("*.wav"):
+        print(f"Extracting features from {file}...")
+        df = extract_features(file, file.stem)
+        data_frames.append(df)
+
+    # combine all df's in one array
+    final_df = pd.concat(data_frames, ignore_index=True)
+
+    # create feature labels for data frame (col names)
+    feature_names = (
+    [f"mfcc_{i}" for i in range(1, 14)] +
+    [f"chroma_{i}" for i in range(1, 13)] +
+    [f"contrast_{i}" for i in range(1, 8)] +
+    ["zcr", "rms"] +
+    [f"mfcc_delta_{i}" for i in range(1, 14)] +
+    ["speaker"]
+    )
+
+    final_df.columns = feature_names
+    final_df.to_csv(OUTPUT_PATH, index=False)
+    print("Audio Data Successfully Processed")
+
+if __name__ == '__main__':
+    main()
+
