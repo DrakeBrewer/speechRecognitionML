0d7e72194f401d2443903a41411ab15ea22f290e
BrownChickenBrownCow <david.birney@wsu.edu>
2025-11-15 12:12:36 -0800

    updated bash script and removed all m4a files/folders

--- scripts/m4a_2_wav.sh
+++ scripts/m4a_2_wav.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 curr_dir="$(dirname "$0")"
-audio_dir="${curr_dir}/../data/audio/raw"
+audio_dir="${curr_dir}/../data/audio/raw/"
 
-for dir in $audio_dir; do
+for dir in "$audio_dir"*/; do
 	file=$(find "$dir" -type f -name "*.m4a" | head -n 1)
 	if [ -n "$file" ]; then
-		speaker=$(basename "$dir")
-		output="${audio_dir}/${speaker}.wav"
+		speaker=$(basename "$file" .m4a)
+		output="${audio_dir}${speaker}.wav"
 		
 		if [ -f "$output" ]; then
 			echo "skipping $speaker"
 			continue
 		fi
 
-		ffmpeg -i "$file" -ac 1 -ar 22050 "$output"
+		ffmpeg -v quiet -i "$file" -ac 1 -ar 22050 "$output"
 		echo "Converted $speaker"
 	fi
 done
