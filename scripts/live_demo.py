12c370788cc35a05b92458ecec2d9dcde28b0a41
BrownChickenBrownCow <david.birney@wsu.edu>
2025-11-19 15:06:46 -0800

    add live_demo.py script

--- scripts/live_demo.py
+++ scripts/live_demo.py
@@ -1,29 +1,55 @@
+import sys
 import sounddevice as sd
 import soundfile as sf
-import numpy as np
 from joblib import load
 from pathlib import Path
-from src.speech_recognition.utils.extract_audio_features import get_feature_names, extract_features
+
+# add src as module for importing
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DEMO_DIR = PROJECT_ROOT / "data" / "audio" / "live"
+sys.path.append(str(PROJECT_ROOT / "src"))
+from speech_recognition.utils import extract_audio_features as eaf
 
 
-# load pickled model
+def record(duration=1.0):
+    sr = eaf.SAMPLE_RATE
+    out_file = DEMO_DIR / "demo.wav"
+    recording = sd.rec(int(sr * duration), samplerate=sr, channels=1)
+    sd.wait()                          # wait for recording to finish
+    sf.write(out_file, recording, sr)  # overwritten each loop
+    return out_file
 
-def record_1sec():
-    pass
-
-def predict_one_instance(file_path):
-    df = extract_features(file_path)
-    # if df empty -> print("No speaker present")
-    # else, add feature names, drop col ['speaker'], then model.predict(df)
-    # print results
-    pass
 
 def main():
-
+    model = load(PROJECT_ROOT / "src" / "speech_recognition" / "model" / "speech_rec_model.pkl")
+    index_2_speaker = {
+        0: "Drake",
+        1: "Melissa",
+        2: "Lisa",
+        3: "Dan",
+        4: "David"
+    }
 
     while True:
-        wav_file = record_1sec()
-        predict_one_instance(wav_file)
+        # record 1 sec of audio
+        wav_file = record(2.0)
+        df = eaf.extract_features(wav_file, "unknown")
+
+        # if empty -> no audio was recorded -> continue
+        if df.empty:
+            print("No Speaker Detected")
+        else:
+            # if audio recorded -> add feature names, drop target, and then use model to predict speaker
+            df.columns = eaf.get_feature_names()
+            xs = df.drop(columns=['speaker'])
+
+            probs = model.predict_proba(xs)
+            avg = probs.mean(axis=0)
+            best_idx = avg.argmax()
+            predicted = index_2_speaker[model.classes_[best_idx]]
+            certainty = avg[best_idx]
+            print(f"Speaker: {predicted} | Certainty: {certainty:.2f}")
+
 
 if __name__ == '__main__':
     main()
