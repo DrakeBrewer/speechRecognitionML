013e824aea78c542592d801b9324fed8cf855f2d
BrownChickenBrownCow <david.birney@wsu.edu>
2025-11-28 13:56:58 -0800

    update notebook + pkl + live_demo.py

--- scripts/live_demo.py
+++ scripts/live_demo.py
@@ -9,57 +9,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DEMO_DIR = PROJECT_ROOT / "data" / "audio" / "live"
 sys.path.append(str(PROJECT_ROOT / "src"))
 from speech_recognition.utils import extract_audio_features as eaf
-
-
-def record(duration=2.0, file_name="live_demo"):
-    sr = eaf.SAMPLE_RATE
-    out_file = DEMO_DIR / f"{file_name}.wav"
-
-    # Warm-up read
-    sd.rec(int(0.05 * sr), samplerate=sr, channels=1, dtype="float32")
-    sd.wait()
-
-    # Real recording
-    print(f"Recording {file_name.name} for {duration} seconds ...")
-    recording = sd.rec(int(sr * duration), samplerate=sr, channels=1, dtype="float32")
-    sd.wait()
-
-    sf.write(out_file, recording, sr, subtype='PCM_16')
-    print(f"Saved recording to {out_file}")
-    return out_file
-
-
-def live_demo():
-    pipeline = load(PROJECT_ROOT / "notebooks" / "speech_rec_model.pkl")
-    index_2_speaker = {
-        0: "Drake",
-        1: "Melissa",
-        2: "Lisa",
-        3: "Dan",
-        4: "David"
-    }
-
-    while True:
-        # record 1 sec of audio
-        wav_file = record(2.0, "demo")
-        df = eaf.extract_features(wav_file, "unknown")
-
-        # if empty -> no audio was recorded -> continue
-        if df.empty:
-            print("No Speaker Detected")
-        else:
-            # if audio recorded -> add feature names, drop target, and then use model to predict speaker
-            df.columns = eaf.get_feature_names()
-            xs = df.drop(columns=['speaker'])
-            print(df.to_string())
-
-            probs = pipeline.predict_proba(xs)
-            avg = probs.mean(axis=0)
-            best_idx = avg.argmax()
-            predicted = index_2_speaker[pipeline.classes_[best_idx]]
-            certainty = avg[best_idx]
-            print(f"Speaker: {predicted} | Certainty: {certainty:.2f}")
-            break
+from speech_recognition.model import model
 
 def main():
     # pipeline = load(PROJECT_ROOT / "notebooks" / "speech_rec_model.pkl")
